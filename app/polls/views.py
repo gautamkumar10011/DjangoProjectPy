043f242a6c34063dbcccf2470fa356a08b670104
gautamkumar10011 <58223009+gautamkumar10011@users.noreply.github.com>
2021-11-20 06:02:23 +0530

    Its flak8

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-## Create your views here.
-## Link of learning
-## https://github.com/veryacademy/YT-Django-GitHub-Actions-
-## Testing/blob/main/.github/workflows/testing.yml
+# Create your views here.
+# Link of learning
+# https://github.com/veryacademy/YT-Django-GitHub-Actions-
+# Testing/blob/main/.github/workflows/testing.yml
