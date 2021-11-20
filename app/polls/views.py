90b9ba4629d966c835a7340924f5310359ede39d
gautamkumar10011 <58223009+gautamkumar10011@users.noreply.github.com>
2021-11-20 05:44:42 +0530

    views.py updated

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+## Create your views here.
+## Link of learning
+## https://github.com/veryacademy/YT-Django-GitHub-Actions-Testing/blob/main/.github/workflows/testing.yml
